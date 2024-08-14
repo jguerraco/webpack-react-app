b9b4db294f79a59585b38480130f15dbf48b1f76
@jose.guerra-conterras <jose.guerra-conterras@bps.uk.atos.net>
2024-08-14 10:52:17 +0100

    updated config

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,78 @@
-const path = require("path");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { merge } = require('webpack-merge');
 
-module.exports = {
-  entry: "./src/index.tsx",
+// Common configuration for both environments
+const commonConfig = {
+  entry: './src/index.tsx',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js"],
+    extensions: ['.tsx', '.ts', '.js'],
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: "ts-loader",
+        use: 'ts-loader',
         exclude: /node_modules/,
       },
       {
-        test: /\.s[ac]ss$/i,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+  ],
+};
+
+// Development-specific configuration
+const devConfig = {
+  mode: 'development',
+  devtool: 'inline-source-map',
   devServer: {
-    static: {
-      directory: path.join(__dirname, "public"),
-    },
-    port: 3000,
+    static: './dist',
+    hot: true,
+    open: true,
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+  ],
+};
+
+// Production-specific configuration
+const prodConfig = {
+  mode: 'production',
+  devtool: 'source-map',
+  optimization: {
+    minimize: true,
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+  ],
+};
+
+// Export the merged configuration based on the environment
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  const config = isProduction ? prodConfig : devConfig;
+  return merge(commonConfig, config);
 };
